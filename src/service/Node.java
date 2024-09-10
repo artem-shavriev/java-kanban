6b16adf309af5c096755021be7ec584077b35b7a
Artem Shavriev <artem-shavriev@yandex.ru>
2024-09-10 11:03:11 +0300

    ФЗ 6. Исправить code style.

--- src/service/Node.java
+++ src/service/Node.java
@@ -1,11 +1,11 @@
 package service;
 
-public class Node <T> {
+public class Node<T> {
     public T data;
-    public Node <T> next;
-    public Node <T> prev;
+    public Node<T> next;
+    public Node<T> prev;
 
-    public Node(Node <T> prev, T data, Node<T> next) {
+    public Node(Node<T> prev, T data, Node<T> next) {
         this.data = data;
         this.next = next;
         this.prev = prev;
