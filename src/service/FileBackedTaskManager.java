b72b2714ffeea9e37b896c17c5394f011c441ca1
Artem Shavriev <artem-shavriev@yandex.ru>
2024-09-18 11:23:55 +0300

    ФЗ 7. Переопределеить модифицирующие операции.

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -1,5 +1,108 @@
 package service;
 
-public class FileBackedTaskManager extends InMemoryTaskManager {
+import model.Epic;
+import model.Subtask;
+import model.Task;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    Path  backedFile;
+
+    public FileBackedTaskManager(Path backedFile) {
+        this.backedFile = backedFile;
+    }
+
+    public void save() {
+    }
+
+    @Override
+    public Task addTask(Task task) {
+        super.addTask(task);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic addEpic(Epic epic) {
+        super.addEpic(epic);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Subtask addSubtask(Subtask subtask) {
+        super.addSubtask(subtask);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        super.updateTask(task);
+        save();
+        return task;
+    }
+
+    @Override
+    public Epic updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+        return epic;
+    }
+
+    @Override
+    public Subtask updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+        save();
+        return subtask;
+    }
+
+    @Override
+    public Task removeTaskById(int id) {
+        super.removeTaskById(id);
+        save();
+        return super.getTaskById(id);
+    }
+
+    @Override
+    public Subtask removeSubtaskById(int id) {
+        super.removeSubtaskById(id);
+        save();
+        return super.getSubtaskById(id);
+    }
+
+    @Override
+    public Epic removeEpicById(int id) {
+        super.removeEpicById(id);
+        save();
+        return super.getEpicById(id);
+    }
+
+    @Override
+    public void removeTasks() {
+        super.removeTasks();
+        save();
+    }
+
+    @Override
+    public void removeEpics() {
+        super.removeEpics();
+        save();
+    }
+
+    @Override
+    public void removeSubtasks() {
+        super.removeSubtasks();
+        save();
+    }
 }
