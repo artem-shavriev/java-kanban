a150f44715b181b4562cb28b2a1d24cc6eb47b2b
Artem Shavriev <artem-shavriev@yandex.ru>
2024-09-19 14:35:59 +0300

    ФЗ 7. - Исправить ошибки pull Requests

--- src/service/ManagerSaveException.java
+++ src/service/ManagerSaveException.java
@@ -2,7 +2,7 @@ package service;
 
 import java.io.IOException;
 
-public class ManagerSaveException extends RuntimeException{
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(IOException e) {
     }
 }
